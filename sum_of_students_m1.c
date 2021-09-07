89a68b09c60bc27cd6c90c5e411b1c171261d9ab
Bharath m.s <46149306+madhurubharath@users.noreply.github.com>
2021-09-07 22:51:40 +0530

    Update sum_of_students_m1.c
    added qustgion

--- sum_of_students_m1.c
+++ sum_of_students_m1.c
@@ -1,3 +1,27 @@
+/*You are given an array of integers, , denoting the marks scored by students in a class.
+
+The alternating elements marks1 , marks2,marks3  and so on denote the marks of boys.
+Similarly, , ,  and so on denote the marks of girls.
+The array name, , works as a pointer which stores the base address of that array. In other words,  contains the address where  is stored in the memory.
+
+For example, let  and  stores 0x7fff9575c05f. Then, 0x7fff9575c05f is the memory address of .
+
+image
+
+Function Description
+
+Complete the function, marks_summation in the editor below.
+
+marks_summation has the following parameters:
+
+int marks[number_of_students]: the marks for each student
+int number_of_students: the size of marks[]
+char gender: either 'g' or 'b*/
+
+
+
+
+
 int marks_summation(int* marks, int number_of_students, char gender) 
 {
   //Write your code here.
